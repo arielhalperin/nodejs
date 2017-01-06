1baaf1cc4e235e7017883fd81e10c3028c318bac
halperinariel <halperinariel@gmail.com>
2017-01-06 15:39:30 +0200

    level 64

---
+++ routes/messages.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var Message = require('../models/Message');
+
+router.get('/', function (req, res, next) {
+    Message.find()
+        .exec(function(err, messages){
+            if(err){
+                return res.status(500).json({
+                    title: 'An error Occured',
+                    error: err
+                });
+            }
+
+            res.status(200).json({
+                message: "Success",
+                obj: messages
+            });
+        });
+});
+
+router.post('/', function (req, res, next) {
+    var message = new Message({
+        content: req.body.content
+    });
+
+    message.save(function(err, result){
+        if(err){
+            return res.status(500).json({
+                title: 'An error Occured',
+                error: err
+            });
+        }
+
+        res.status(201).json({
+           message: "Saved",
+           obj: result
+        });
+    });
+});
+
+module.exports = router;
