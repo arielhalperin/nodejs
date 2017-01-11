030b9cc3e5d9d3a8235539d574f18a18063e8554
arielh <halperinariel@gmail.com>
2017-01-11 19:05:41 +0200

    level 73

---
+++ assets/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import {Http, Headers, Response} from "@angular/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import 'rxjs/Rx';
+
+import {User} from "./user.model";
+
+@Injectable()
+export class AuthService{
+
+    constructor(private http: Http){}
+
+    signup(user: User){
+        const body = JSON.stringify(user);
+        console.log(body);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        //noinspection TypeScriptUnresolvedFunction
+        return this.http.post('http://localhost:3000/user',body,{headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+   }
+}
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var jwt =  require('jsonwebtoken');
 
 router.post('/', function (req, res, next) {
     var user = new User({
         firstName: req.body.firstName,
-        lastname: req.body.lastname,
+        lastName: req.body.lastName,
         password: bcrypt.hashSync(req.body.password, 10),
         email: req.body.email
     });
@@ -21,6 +22,38 @@ router.post('/', function (req, res, next) {
         res.status(201).json({
             message: "User created",
             obj: result
+        });
+    });
+});
+
+router.post('/signin', function (req, res, next) {
+    User.findOne({email: req.body.email}, function(err, user){
+        if (err) {
+            return res.status(500).json({
+                title: 'An error Occured',
+                error: err
+            });
+        }
+
+        if(!user){
+            return res.status(401).json({
+                title: 'Login Failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+
+        if(!bcrypt.compareSync(req.body.password, user.password)){
+            return res.status(401).json({
+                title: 'Login Failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
         });
     });
 });
